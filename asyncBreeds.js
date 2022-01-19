83b73bc07113680371f75e5b4e58836a6e8c3934
Casey <caseyjtite@gmail.com>
2022-01-18 17:30:09 -0800

    working on cats async test

---
+++ asyncBreeds.js
@@ -0,0 +1,27 @@
+// asyncBreeds.js
+const fs = require("fs");
+// const breedDetails = require("./syncBreeds");
+
+const breedDetailsFromFile = function (breed, callbackAfterFunctionIsOver) {
+  // console.log("one");
+  console.log("breedDetailsFromFile: Calling readFile...");
+  fs.readFile(`./data/${breed}.txt`, "utf8", (error, data) => {
+    // console.log("three");
+    // console.log("In readFile's Callback: it has the data.");
+    // ISSUE: Returning from *inner* callback function, not breedDetailsFromFile.
+    if (!error) callbackAfterFunctionIsOver(data);
+  });
+  // console.log("two");
+  // ISSUE: Attempting to return data out here will also not work.
+  //        Currently not returning anything from here, so breedDetailsFromFile function returns undefined.
+  // return "hello";
+};
+
+const printBreed = (breed) => console.log(breed);
+
+// // we try to get the return value
+const bombay = breedDetailsFromFile("Bombay", printBreed);
+// // console.log("Return Value: ", bombay); // => will NOT print out details, instead we will see undefined
+
+module.exports = breedDetailsFromFile;
+// module.exports = printBreed;
---
+++ test/syncBreedsTest.js
@@ -0,0 +1,24 @@
+const assert = require("chai").assert;
+const breedDetails = require("../syncBreeds");
+
+// describe("#breedDetails", () => {
+//   it("returns breed details for the bombay breed", () => {
+//     //
+//     const expected =
+//       "The golden eyes and the shiny black coat of the Bombay is absolutely striking. Likely to bond most with one family member, the Bombay will follow you from room to room and will almost always have something to say about what you are doing, loving attention and to be carried around, often on his caregiver's shoulder.";
+
+//     const actual = breedDetails("Bombay");
+//     assert.equal(expected, actual);
+//   });
+//   it("returns breed details for balinese breed", () => {
+//     const expected =
+//       "Balinese are curious, outgoing, intelligent cats with excellent communication skills. They are known for their chatty personalities and are always eager to tell you their views on life, love, and what you’ve served them for dinner.";
+//     const actual = breedDetails("Balinese");
+//     assert.equal(expected, actual);
+//   });
+//   it("should return undefined when no breed is specified", () => {
+//     const expected = undefined;
+//     const actual = breedDetails();
+//     assert.equal(expected, actual);
+//   });
+// });
